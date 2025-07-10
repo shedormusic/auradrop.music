9e1ff227361cda170477b94c5911cec808efd2d1
shedormusic <sheptulin5558@gmail.com>
2025-07-10 03:10:57 +0300

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,38 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const token = "7780266328:AAGHL0uAigkFJu43nVlekat0vdnyUbT4l7M";
+  const chatId = "1885891403";
+
+  const formData = new FormData(e.target);
+
+  let message = "<b>📀 Новый релиз на AuraDrop</b>\n\n";
+
+  formData.forEach((value, key) => {
+    message += `<b>${key}:</b> ${value}\n`;
+  });
+
+  fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message,
+      parse_mode: "HTML",
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        alert("✅ Отправлено!");
+        e.target.reset();
+      } else {
+        alert("❌ Ошибка при отправке.");
+      }
+    })
+    .catch((err) => {
+      alert("❌ Не удалось подключиться.");
+      console.error(err);
+    });
+});
